07fe9fbb5f7cf7b9ca4c19eef30882a65aef46bf
Ken Smith <fwxgq.oysqv@fastmail.com>
2025-02-10 11:53:49 -0800

    better graceful degrataion

--- sandbox/ai.mk
+++ sandbox/ai.mk
@@ -1,6 +1,6 @@
 # include this makefile from your project makefile to add a target called 'ai'
 # which will send question.md to the LLM providers listed in $(use-models)
-#
+
 # usage:
 #
 # cat << EOF >> GNUmakefile
@@ -11,35 +11,39 @@
 # EOF
 # make ai
 
-MAKEFLAGS = -j
-
-ai := $(wildcard $(CURDIR)/../ai)
-$(if $(strip $(ai)), \
-  $(comment binary is built), \
-  $(eval ai := $(shell which ai)) \
-  $(if $(strip $(ai)), \
-    $(comment binary found in environment), \
-    $(error couldn't find ai binary, please build or install it) \
-   ) \
- )
-
 use-models := \
   claude-3-5-sonnet-latest \
   gpt-4o \
   grok-2-latest
 
-.PHONY: ai
-ai:
-	cat question.md answer*
+MAKEFLAGS = -j
 
-$(foreach model,$(use-models), \
-  $(eval .PHONY: $(model)-target) \
-  $(eval \
-    $(model)-target \
-    : $(ai) \
-    ; cat question.md | ai $(model) > answer-$(model).md \
-  ) \
-  $(eval \
-    ai: $(model)-target \
+ai := $(wildcard $(CURDIR)/../ai)
+$(if $(strip $(ai)), \
+  $(comment binary is built), \
+  $(eval ai := $(shell which ai 2>/dev/null)) \
+  $(if $(strip $(ai)), \
+    $(comment binary found in environment), \
+    $(comment the ai target will complain about a missing ai binary) \
+   ) \
+ )
+
+.PHONY: ai
+$(if $(strip $(ai)), \
+  $(eval ai:;cat question.md answer*), \
+  $(eval ai:;@echo "please install the ai binary with 'go install github.com/kensmith/ai@latest'") \
+ )
+
+$(if $(strip $(ai)), \
+  $(foreach model,$(use-models), \
+    $(eval .PHONY: $(model)-target) \
+    $(eval \
+      $(model)-target \
+      : $(ai) \
+      ; cat question.md | ai $(model) > answer-$(model).md \
+    ) \
+    $(eval \
+      ai: $(model)-target \
+     ) \
    ) \
  )
